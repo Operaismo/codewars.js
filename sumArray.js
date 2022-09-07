65a8291b703279b567dd4283cae10a59521813b1
Operaismo <eloperaismo@protonmail.com>
2022-09-07 16:35:54 -0500

    feat: implemented sumArray 8kyu

---
+++ sumArray.js
@@ -0,0 +1,8 @@
+function sumArray(array)  {
+    if(array && array.length > 1) {
+        const sortedArray = array.sort((a,b) => a - b).slice(1, -1);
+        return sortedArray.reduce((acc, cur) => acc + cur, 0);
+    }
+
+    return 0;
+}

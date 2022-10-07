49be3701e2a198c05c9a118ac1370139d5a2943c
Operaismo <eloperaismo@protonmail.com>
2022-10-07 18:11:16 -0500

    feat: implemented arrayDiff 6kyu

--- 5kyu/moveZeros.js
+++ 5kyu/moveZeros.js
@@ -1,4 +1,11 @@
 function moveZeros(arr) {
-  const nonZero = arr.filter(x => x != 0 || typeof x === 'boolean' || typeof x === 'string' || typeof x === null || Array.isArray(x));
+  const nonZero = arr.filter(
+    (x) =>
+      x != 0 ||
+      typeof x === 'boolean' ||
+      typeof x === 'string' ||
+      typeof x === null ||
+      Array.isArray(x)
+  );
   return [...nonZero, ...new Array(arr.length - nonZero.length).fill(0)];
 }

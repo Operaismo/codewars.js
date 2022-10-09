0e54bb1241d9c1b2a0946bc17b56b4193e809f05
Operaismo <eloperaismo@protonmail.com>
2022-10-09 15:38:01 -0500

    feat: implemented simpleMultiplication 8kyu

--- 8kyu/bmi.js
+++ 8kyu/bmi.js
@@ -1,6 +1,12 @@
 function bmi(weight, height) {
   let result = weight / (height ** 2);
-  if (result <= 18.5){
+  if (result <= 18.5) {
     return "Underweight";
+  } else if (result <= 25.0) {
+    return "Normal";
+  } else if (result <= 30.0) {
+    return "Overweight";
+  } else {
+    return "Obese";
   }
 }

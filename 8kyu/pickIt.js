f78b2924d66d042996e084b7c3d2f569fd14868f
Operaismo <mattjbum@protonmail.com>
2022-10-14 19:45:50 -0500

    feat: implemented pickIt 8kyu

---
+++ 8kyu/pickIt.js
@@ -0,0 +1,9 @@
+function pickIt(arr) {
+  var odd = [], even = [];
+  //coding here
+  for(let i = 0; i < arr.length; i++){
+    if(arr[i] % 2) {odd.push(arr[i])} else {even.push(arr[i])};
+  }
+
+  return [odd, even];
+}

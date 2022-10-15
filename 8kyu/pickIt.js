81bfb377a9a6e49724fd1db237b7ad15d61c5498
Operaismo <mattjbum@protonmail.com>
2022-10-14 20:04:20 -0500

    chore: used prettier formatter for pickIt.js

--- 8kyu/pickIt.js
+++ 8kyu/pickIt.js
@@ -1,7 +1,7 @@
 function pickIt(arr) {
   var odd = [], even = [];
-  for(let i = 0; i < arr.length; i++){
-    if(arr[i] % 2) {odd.push(arr[i])} else {even.push(arr[i])};
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] % 2) { odd.push(arr[i]) } else { even.push(arr[i]) };
   }
   return [odd, even];
 }

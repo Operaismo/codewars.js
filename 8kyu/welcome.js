893fb7fd7f3a573e664acd185071e6b083c42d25
Operaismo <mattjbum@protonmail.com>
2022-10-18 18:04:12 -0500

    feat: implemented welcome 8kyu

---
+++ 8kyu/welcome.js
@@ -0,0 +1,22 @@
+function greet(language) {
+  const database = {
+    english: "Welcome",
+    czech: "Vitejte",
+    danish: "Velkomst",
+    dutch: "Welkom",
+    estonian: "Tere tulemast",
+    finnish: "Tervetuloa",
+    flemish: "Welgekomen",
+    french: "Bienvenue",
+    german: "Willkommen",
+    irish: "Failte",
+    italian: "Benvenuto",
+    latvian: "Gaidits",
+    lithuanian: "Laukiamas",
+    polish: "Witamy",
+    spanish: "Bienvenido",
+    swedish: "Valkommen",
+    welsh: "Croeso",
+  };
+  return database[language] || base.english;
+}

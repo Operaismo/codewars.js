ed10db5f08814e44273c4d9e64c93a2f215441a2
Operaismo <mattjbum@protonmail.com>
2022-10-20 18:43:40 -0500

    fix: reformatted updateLight with Prettier

--- 8kyu/updateLight.js
+++ 8kyu/updateLight.js
@@ -1,13 +1,13 @@
 function updateLight(current) {
-    switch(current){
-        case "green":
-          return "yellow";
-          break;
-        case "yellow":
-          return "red";
-          break;
-        case "red":
-          return "green";
-          break;
-    }
+  switch (current) {
+    case "green":
+      return "yellow";
+      break;
+    case "yellow":
+      return "red";
+      break;
+    case "red":
+      return "green";
+      break;
   }
+}

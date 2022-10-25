3a8ca704057e0b06670cab0cee05963a083cbb5e
Operaismo <mattjbum@protonmail.com>
2022-10-25 02:59:41 -0500

    feat: implemented rps 8kyu

---
+++ 8kyu/rps.js
@@ -0,0 +1,14 @@
+function rps(p1, p2) {
+  if (p1 === p2) {
+    return `Draw!`;
+  }
+  if (p1 === 'rock' && p2 === 'scissors') {
+    return `Player 1 won!`;
+  } else if (p1 === 'paper' && p2 === 'rock') {
+    return `Player 1 won!`;
+  } else if (p1 === 'scissors' && p2 === 'paper') {
+    return `Player 1 won!`;
+  } else {
+    return `Player 2 won!`;
+  }
+}

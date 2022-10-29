725b03c9afc1087b7a969676225ee73653e2c349
Operaismo <mattjbum@protonmail.com>
2022-10-29 02:31:22 -0500

    feat: implemented areaOrPerimeter and DNAtoRNA 8kyu and DirReduc 5kyu

--- 5kyu/dirReduc.js
+++ 5kyu/dirReduc.js
@@ -1,4 +1,14 @@
-function dirReduc(arr){
-    const opposites = {'NORTH': 'SOUTH', 'EAST': 'WEST', 'SOUTH': 'NORTH', 'WEST': 'EAST'};
-    return arr.reduce((acc, cur) => (opposites[acc.slice(-1)] === cur ? acc.pop() : acc.push(cur), acc), []);
+function dirReduc(arr) {
+  const opposites = {
+    NORTH: "SOUTH",
+    EAST: "WEST",
+    SOUTH: "NORTH",
+    WEST: "EAST",
+  };
+  return arr.reduce(
+    (acc, cur) => (
+      opposites[acc.slice(-1)] === cur ? acc.pop() : acc.push(cur), acc
+    ),
+    []
+  );
 }

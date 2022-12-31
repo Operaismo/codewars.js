b51412ae6a51d64aa26420b5f094dff193859423
Operaismo <eloperaismo@protonmail.com>
2022-12-31 17:39:51 -0600

    fix: fixed bug where arr was not initialized

--- 6kyu/uniqueInOrder.js
+++ 6kyu/uniqueInOrder.js
@@ -1,8 +1,9 @@
 var uniqueInOrder = function (iterable) {
-  for (var i = 0; i < iterable.length; i++) {
-    if (iterable[i] !== iterable[i + 1]) {
-      arr.push(iterable[i]);
+    let arr = [];
+    for (var i = 0; i < iterable.length; i++) {
+      if (iterable[i] !== iterable[i + 1]) {
+        arr.push(iterable[i]);
+      }
     }
+    return arr;
   }
-  return arr;
-}

94f26ba1c2b30d89bafb473535a33764572ee1b6
Operaismo <eloperaismo@protonmail.com>
2022-12-30 19:10:39 -0600

    feat: implemented weatherInfo 8kyu

---
+++ 8kyu/weatherInfo.js
@@ -0,0 +1,12 @@
+function weatherInfo (temp) {
+  let c = convertToCelsius(temp);
+  if (c <= 0)
+    return (c + " is freezing temperature");
+  else
+    return (c + " is above freezing temperature");
+}
+
+function convertToCelsius (temperature) {
+  var celsius = (temperature - 32) * (5/9);
+  return celsius;
+}

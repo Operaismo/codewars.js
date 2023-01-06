640bbb226ac8feb7587f20a35f6e915ea086f35b
Operaismo <mattybcodes@gmail.com>
2023-01-06 16:20:18 -0600

    feat: added alternative implementation of padIt 8kyu

--- 8kyu/padIt.js
+++ 8kyu/padIt.js
@@ -1,10 +1,11 @@
 function padIt(str, n) {
   while (n > 0) {
-    if (n % 2 === 0) {
-      str = str + '*';
-    } else {
-      str = '*' + str;
-    }
+    // if (n % 2 === 0) {
+    //   str = str + '*';
+    // } else {
+    //   str = '*' + str;
+    // }
+    n % 2 === 0 ? str = str + '*' : str = '*' + str;
     n--;
   }
   return str;

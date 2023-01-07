b066e15e690c44758d62139c9cc1447e4890578a
Operaismo <mattybcodes@gmail.com>
2023-01-07 16:36:50 -0600

    chore: removed excess comments

--- 8kyu/padIt.js
+++ 8kyu/padIt.js
@@ -1,10 +1,5 @@
 function padIt(str, n) {
   while (n > 0) {
-    // if (n % 2 === 0) {
-    //   str = str + '*';
-    // } else {
-    //   str = '*' + str;
-    // }
     n % 2 === 0 ? str = str + '*' : str = '*' + str;
     n--;
   }

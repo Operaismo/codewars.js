81bbff0ac90d63584c755edce8abfd2f050c63ba
Operaismo <mattybcodes@gmail.com>
2023-01-10 18:02:43 -0600

    feat: implemented howManydays 8kyu

---
+++ 8kyu/howManydays.js
@@ -0,0 +1,20 @@
+function howManydays(month) {
+  let days;
+  switch (month) {
+    case 2:
+      days = 28;
+      break;
+    case 4:
+    case 6:
+    case 9:
+    case 11:
+      days = 30;
+      break;
+    case 2:
+      days = 28;
+      break;
+    default:
+      days = 31;
+  }
+  return days;
+}

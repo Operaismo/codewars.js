0e39b3927de1f5e0e7335449c3e8fa4db7ca01f3
Operaismo <eloperaismo@protonmail.com>
2023-02-16 23:16:38 -0600

    feat: implemented configuration 8kyu

---
+++ 8kyu/configuration.js
@@ -0,0 +1,8 @@
+const configuration = {
+  "name": "your-pack-name",
+  "version": "6.9.0",
+  "description": "",
+  "license": "MIT"
+};
+// Hint: try creating with `npm init` command
+// Help! -> https://docs.npmjs.com/files/package.json

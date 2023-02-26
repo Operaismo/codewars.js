7e0ca8dde3aaab2ab6dd3e7c27c0dc56a8923153
Operaismo <mattybcodes@gmail.com>
2023-02-25 18:39:22 -0600

    fix: reformatted describeAge with Prettier

--- 8kyu/describeAge.js
+++ 8kyu/describeAge.js
@@ -1,4 +1,3 @@
-function describeAge(a){
-  return `You're a(n) ${a<13?'kid':a<18?'teenager':a<65?'adult':'elderly'}`
-  }
-  
+function describeAge(a) {
+  return `You're a(n) ${a < 13 ? 'kid' : a < 18 ? 'teenager' : a < 65 ? 'adult' : 'elderly'}`
+}

c5d885e0c70653e5e169b4df51e699472b73544e
Operaismo <mattybcodes@gmail.com>
2023-03-10 13:16:24 -0600

    feat: implemented defineSuit 8kyu

---
+++ 8kyu/defineSuit.js
@@ -0,0 +1,9 @@
+function defineSuit(card) {
+   var suit = card.substr(-1);
+   switch (suit) {
+      case '♣': return 'clubs';
+      case '♦': return 'diamonds';
+      case '♥': return 'hearts';
+      default: return 'spades';
+   }
+}

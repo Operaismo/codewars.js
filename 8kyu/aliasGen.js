d2b244e5a7e031d4ed39a7057d35fe3afa966be5
Operaismo <mattybcodes@gmail.com>
2023-06-24 17:02:37 -0500

    refactor(/): added comments in aliasGen

--- 8kyu/aliasGen.js
+++ 8kyu/aliasGen.js
@@ -1,4 +1,4 @@
-// aliasGen 8kyu!!!!
+// aliasGen 8kyu!!!
 function aliasGen(first, last) {
   return first[0].match(/[a-zA-Z]/i) && last[0].match(/[a-zA-Z]/i)
     ? `${firstName[first[0].toUpperCase()]} ${surname[last[0].toUpperCase()]}`
